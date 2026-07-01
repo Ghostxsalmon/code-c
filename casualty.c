1246f6285bdde8e5eba5613dda53c02617b3f439
wyatt <chewebone@gmail.com>
2026-07-01 11:50:33 -0500

    First compile with rand working

--- casualty.c
+++ casualty.c
@@ -17,9 +17,13 @@ int apply_damage();
 
 int main(){
     srand(time(NULL));
+    int marine_wounds = 10;
+    marine_wounds = marine_wounds - apply_damage(); // variable does not use () if you are calling the function then use ()
+    printf("%d\n", marine_wounds);
     return 0;
 }
 
 int apply_damage(){
     int damage = rand() %6 + 1;
+    return damage;
 }
