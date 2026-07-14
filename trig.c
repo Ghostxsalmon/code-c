1271174fd2b7f5e18982292c059a95423eb6997e
wyatt <chewebone@gmail.com>
2026-07-14 01:47:01 -0500

    started trig  project

---
+++ trig.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+#include <math.h>
+
+// These will not come out to whole numbers, I will need to use decimals so i will need float not int.
+float hypotenuse();
+float adjacent();
+float opposite();
+
+int main(void){
+    
+    return 0;
+}
