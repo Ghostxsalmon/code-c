51cf9fb53907864ac1ec15b330edff590473dc41
wyatt <chewebone@gmail.com>
2026-07-15 01:00:39 -0500

    ratio

--- trig.c
+++ trig.c
@@ -5,8 +5,20 @@
 float hypotenuse();
 float adjacent();
 float opposite();
+int ratio();
+
 
 int main(void){
-    
+    int choice = ratio();
+    printf("you have chosen %d", choice);
+
     return 0;
 }
+
+int ratio(){ // char might seem more obvious but it is harder to manage memory so stick with int for now
+    int trig;
+    printf("What are you atttempting?\n\n 1 for sin\n 2 for cos\n 3 for tan\n\n");
+    scanf("%d", &trig);
+    return trig;
+}
+
